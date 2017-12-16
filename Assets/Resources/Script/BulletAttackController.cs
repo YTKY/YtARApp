98c91af0a6a2191617bd78d8e20581b9311ed7e1
Yuki Tsuboi <ytsuboi@vmware.com>
2017-12-16 23:06:29 +0900

    attack implemented

---
+++ Assets/Resources/Script/BulletAttackController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletAttackController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//　コライダのIsTriggerのチェックを外し物理的な衝突をさせる場合
+	void OnCollisionEnter(Collision col) {
+			Destroy(gameObject);
+			Destroy(col.gameObject);
+	}
+}
